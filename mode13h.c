b47c9bcd438a7f09c08bfcb05cf19384b520259f
Ibán Cereijo <ibancg@gmail.com>
2010-12-05 17:24:42 +0100

    (no commit message)

--- mode13h.c
+++ mode13h.c
@@ -1,5 +1,5 @@
 // Mode13h - Mode 13h graphics wrapper for SDL
-// Copyright 1995-2010, Ibán Cereijo Graña <ibancg at gmail dot com>
+// Copyright 1999-2010, Ibán Cereijo Graña <ibancg at gmail dot com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
